4399db60dad86c6b044b4274f021df5c1c9b3f6c
Paul Hertz <ignotus@gmail.com>
2016-04-04 20:33:07 -0500

    Edited names
    Edited names and deleted unused arrays.

--- src/AssignRandomly.java
+++ src/AssignRandomly.java
@@ -6,35 +6,14 @@ import net.paulhertz.util.*;
 
 public class AssignRandomly extends PApplet {
 
-	/*
-	String[] hypers =   {"Grammatron (Mark Amerika)", "World of Awe (Yael Kanarek)", "_Datableeding (Mary-Anne Mez Breeze)", 
-	                     "Anecdoted Topography of Chance (Daniel Spoerri)", "Translucidity (Talan Memmott)", 
-	                     "These Waves of Girls (Caitlin Fisher)"};
-	String[] students = {"Isabel Bogarin", "Gabrielle Bohr", "Diana Chenault", "Mark Deleon", "Anthony Edwards-Bacon", 
-	                     "Brandon Herford", "Ryan Hrovat", "Olivia Jones-Hernandez", "Hyunji Kang", "Timothy Kelly", 
-	                     "Taylor Knight", "Chia Chang Koo (Jack)", "Elliott Mickleburgh", "Diandra Miller", "Miguel Perez", 
-	                     "Heather Platen", "Darcy Provencher", "Emily Simpson", "Samuel Smith", "Ana Treffinger", 
-	                     "Andrew Valenzuela", "Keith Whittaker"};
-	*/
-	/*
-	String[] hypers =   {"World of Awe", "GRAMMATRON", "Paths of Luminous Painting", "Reach", "My Body"};
-
-	String[] students = {"Tim W Adamczyk", "Laura Andrea Campuzano", "Kevin Carey", "Kyrie Alexis Demay", 
-	                     "James George DiGilio Jr.", "William Arthur Eriksson", "Hunter James Foster", 
-	                     "Jeremy Douglas Gloster", "Lisa Greene", "Sam Grossinger", "Sanglim Han", 
-	                     "Brooke Herzing", "Sandy King", "Christopher Michael McLaughlin", "Hayden Worth Myrick", 
-	                     "Lindsey Ellen Neiman", "Tyler Joseph Nieset", "Alison Noggle", "Brian Peterson", 
-	                     "Sana Qazi", "Anna Russett", "JuHyung Ryou", "Anya Hayley Shapiro", "Samuel Curtis Shapiro", 
-	                     "Chelsea Traynor", "Ian Wallach"};
-	                     */
 
 	String[] hypers =   {"Alexi Shulgin", "JODI.org", "Heath Bunting", "Natalie Bookchin", "Young-Hae Chang", "Mouchette"};
 
-	String[] students = {"Yasmine Fouad Aghabi", "Laura Sevall Bise", "Chenyu Chen", "Dylan O'Neill Cooper", 
-	           "Tyler Anthony Thomas Gunn", "Maurice Hampton", "Gabriella Elizabeth Hileman", 
-	           "Kunning Huang", "Caleb Kaiser", "Kacie Elizabeth Lambert", "Somin Lee", "Jonathon Lee McJunkin", 
-	           "Deanna Sandra Musurlian", "Katherine T Rapheal", "Jonathon Scott", "Samantha D Siew", 
-	           "Bobby T Swainhart", "Caitlin Brooke Weres", "Liqian Xu"};
+	String[] students = {"Yasmine", "Laura", "Chenyu", "Dylan", 
+	           "Tyler", "Maurice", "Gabriella", 
+	           "Kunning", "Caleb", "Kacie", "Somin", "Jonathon", 
+	           "Deanna", "Katherine", "Jonathon", "Samantha", 
+	           "Bobby", "Caitlin", "Liqian"};
 
 	String[] ppl = {"A. Bill Miller", "Antonio Roberts", "Kyle Evans", "James Connolly", 
 			"Jason Soliday", "Jeff Kolar", "Joseph Y0lk Chiocchi", "Jon Satrom", 
@@ -43,26 +22,6 @@ public class AssignRandomly extends PApplet {
 			"Melissa Barron", "Lisa Slodki", "Paul Hertz", 
 			"Alfredo Salazar-Caro", "stAllio!", "I <3 Presets", "Channel TWo", "PoxParty"};
 	
-	/*
-
-	///// names only /////
-	"A. Bill Miller", "Antonio Roberts", "Kyle Evans", "James Connolly", 
-	"Jason Soliday", "Jeff Kolar", "Joseph Y0lk Chiocchi", "Jon Satrom", 
-	"Ben Syverson", "Nick Briz", "Nick Kegeyan", "Rob Ray", "Rosa Menkman", 
-	"Curt Cloninger", "Shawné Michelain Holloway", "Jon Cates", 
-	"Melissa Barron", "Lisa Slodki", "Theodore Darst", "Jessica Westbrook", 
-	"Adam Trowbridge", "Paul Hertz", "Alfredo Salazar-Caro", "Benjamin Berg"
-	
-	///// names and identities /////
-	"A. Bill Miller", "Antonio Roberts", "Kyle Evans", "James Connolly", 
-	"Jason Soliday", "Jeff Kolar", "Joseph Y0lk Chiocchi", "Jon Satrom", 
-	"Ben Syverson", "Nick Briz", "Nick Kegeyan", "Rob Ray", "Rosa Menkman", 
-	"Curt Cloninger", "Shawné Michelain Holloway", "Jon Cates", 
-	"Melissa Barron", "Lisa Slodki", "Theodore Darst", "Jessica Westbrook", 
-	"Adam Trowbridge", "Paul Hertz", "Alfredo Salazar-Caro", "Benjamin Berg",
-	"stAllio!", "I <3 Presets", "Channel TWo", "PoxParty"
-
-	 */
 	
 	String[] unichars;
 	
